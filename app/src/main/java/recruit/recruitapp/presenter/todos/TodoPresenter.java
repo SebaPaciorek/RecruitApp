c6de9b5aa95f6ed3e4a31eb75134dccf3a346019
SebaPaciorek <yofreeman@wp.pl>
2018-11-22 00:12:23 +0100

    created todo presenter

---
+++ app/src/main/java/recruit/recruitapp/presenter/todos/TodoPresenter.java
@@ -0,0 +1,10 @@
+package recruit.recruitapp.presenter.todos;
+
+import recruit.recruitapp.contract.todos.TodoListInterface;
+
+public class TodoPresenter implements TodoListInterface.Presenter {
+
+    public TodoPresenter() {
+    }
+
+}
