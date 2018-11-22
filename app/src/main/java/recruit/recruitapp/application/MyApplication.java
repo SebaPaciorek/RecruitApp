478193c7013e6756a2b8cf41bfafb662dabd2419
SebaPaciorek <yofreeman@wp.pl>
2018-11-22 12:34:21 +0100

    declared application

---
+++ app/src/main/java/recruit/recruitapp/application/MyApplication.java
@@ -0,0 +1,13 @@
+package recruit.recruitapp.application;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
