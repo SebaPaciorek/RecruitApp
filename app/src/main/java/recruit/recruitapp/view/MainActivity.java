c9644d1506f2fc03ad1929d2556caf026071ca8d
SebaPaciorek <yofreeman@wp.pl>
2018-11-22 10:34:09 +0100

    corrected todofragment

--- app/src/main/java/recruit/recruitapp/view/MainActivity.java
+++ app/src/main/java/recruit/recruitapp/view/MainActivity.java
@@ -1,15 +1,25 @@
 package recruit.recruitapp.view;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import recruit.recruitapp.R;
+import recruit.recruitapp.view.todos.TodoFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TodoFragment todoFragment = new TodoFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.todoFragmentContainer, todoFragment).commit();
     }
 }
--- app/src/main/java/recruit/recruitapp/view/todos/TodoFragment.java
+++ app/src/main/java/recruit/recruitapp/view/todos/TodoFragment.java
@@ -4,14 +4,28 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import recruit.recruitapp.R;
 import recruit.recruitapp.contract.todos.TodoListInterface;
+import recruit.recruitapp.model.Todo;
+import recruit.recruitapp.presenter.todos.TodoRecyclerViewAdapter;
 
-public class TodoFragment extends Fragment implements TodoListInterface.View{
+public class TodoFragment extends Fragment implements TodoListInterface.View {
+
+    private RecyclerView todoRecyclerView;
+    private List<Todo> todoList;
+
+    private LinearLayoutManager recyclerLayoutManager;
+    private DividerItemDecoration dividerItemDecoration;
 
     public TodoFragment() {
     }
@@ -26,6 +40,36 @@ public class TodoFragment extends Fragment implements TodoListInterface.View{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_todo, container, false);
 
+        todoList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            Todo todo = new Todo();
+            todo.setId(i+1);
+            todo.setUserId(i+1);
+            todo.setCompleted(true);
+            todo.setTitle("Todo" + i);
+            todoList.add(todo);
+        }
+
+        TodoRecyclerViewAdapter todoRecyclerViewAdapter = new TodoRecyclerViewAdapter(todoList);
+
+        todoRecyclerView = view.findViewById(R.id.todoRecyclerView);
+
+        setLayoutManager();
+        setDividerItemDecoration();
+
+        todoRecyclerView.setAdapter(todoRecyclerViewAdapter);
+
         return view;
+    }
+
+    public void setLayoutManager() {
+        recyclerLayoutManager = new LinearLayoutManager(getContext());
+        todoRecyclerView.setLayoutManager(recyclerLayoutManager);
+    }
+
+    public void setDividerItemDecoration() {
+        dividerItemDecoration = new DividerItemDecoration(todoRecyclerView.getContext(), recyclerLayoutManager.getOrientation());
+        todoRecyclerView.addItemDecoration(dividerItemDecoration);
     }
 }
