2baee11a5fb0489ab89a77b664aa04be4281912d
SebaPaciorek <yofreeman@wp.pl>
2018-11-22 12:35:27 +0100

    added getting all todos

--- app/src/main/java/recruit/recruitapp/presenter/todos/TodoPresenter.java
+++ app/src/main/java/recruit/recruitapp/presenter/todos/TodoPresenter.java
@@ -1,12 +1,19 @@
 package recruit.recruitapp.presenter.todos;
 
+import android.util.Log;
+
 import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import io.realm.Realm;
+import io.realm.RealmResults;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import recruit.recruitapp.contract.todos.TodoListInterface;
+import recruit.recruitapp.model.Todo;
+import recruit.recruitapp.model.TodoList;
 import recruit.recruitapp.service.TodoService;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,6 +28,9 @@ public class TodoPresenter implements TodoListInterface.Presenter {
     private TodoService todoService;
 
     private static TodoPresenter todoPresenter;
+
+    private Realm realm;
+    private TodoList todoList;
 
     public TodoPresenter() {
     }
@@ -52,6 +62,16 @@ public class TodoPresenter implements TodoListInterface.Presenter {
             @Override
             public void onResponse(Call<ArrayList<JsonObject>> call, Response<ArrayList<JsonObject>> response) {
                 if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        realm = Realm.getDefaultInstance();
+                        createTodoListObject();
+
+                        for (int i = 0; i < response.body().size(); i++) {
+                            JsonObject todoJsonObject = response.body().get(i);
+
+                            createTodoObject(todoJsonObject);
+                        }
+                    }
 
                 }
             }
@@ -62,6 +82,56 @@ public class TodoPresenter implements TodoListInterface.Presenter {
             }
         });
 
+    }
+
+    @Override
+    public List<Todo> getTodoList() {
+        List<Todo> todoArrayList = new ArrayList<>();
+        realm = Realm.getDefaultInstance();
+
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                RealmResults<TodoList> listOfTodoList = realm.where(TodoList.class).findAll();
+                TodoList todoList = listOfTodoList.get(0);
+
+                for (int i = 0; i < todoList.getTodoRealmList().size(); i++) {
+                    Log.d("LOG", todoList.getTodoRealmList().get(i).getTitle());
+                    todoArrayList.add(todoList.getTodoRealmList().get(i));
+                }
+
+            }
+        });
+
+        return todoArrayList;
+    }
+
+    private void createTodoObject(JsonObject todoJsonObject) {
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Todo todo = realm.createObject(Todo.class);
+                todo.setUserId(todoJsonObject.get("userId").getAsInt());
+                todo.setId(todoJsonObject.get("id").getAsInt());
+                todo.setTitle(todoJsonObject.get("title").getAsString());
+                todo.setCompleted(todoJsonObject.get("completed").getAsBoolean());
+
+                addToTodoList(todo);
+            }
+        });
+    }
+
+    private void createTodoListObject() {
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                todoList = realm.createObject(TodoList.class);
+            }
+        });
+    }
+
+    private void addToTodoList(Todo todo) {
+        todoList.getTodoRealmList().add(todo);
     }
 
     //ensure singleton
