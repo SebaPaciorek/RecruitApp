30c0f3545c7aef876d61e877b356373df0521926
SebaPaciorek <yofreeman@wp.pl>
2018-11-23 15:39:38 +0100

    added new methods to interface for removing object

--- app/src/main/java/recruit/recruitapp/contract/todos/TodoListInterface.java
+++ app/src/main/java/recruit/recruitapp/contract/todos/TodoListInterface.java
@@ -7,12 +7,26 @@ import recruit.recruitapp.model.TodoList;
 
 public interface TodoListInterface {
     interface View {
+        void showAlertDialogRemove(int userId, int id);
 
+        void showProgressBar(boolean show);
+
+        void itemRemoved(int position);
+
+        void showToast(String message);
+
+        void setFirstLaunchSharedPreferences(String firstLaunch);
+
+        void showTodosFirstLaunch(List<Todo> todoList);
     }
 
     interface Presenter {
         void getTodos();
 
         List<Todo> getTodoList();
+
+        List<Todo> getTodoListFirstLaunch();
+
+        void removeTodo(int userId, int id);
     }
 }
